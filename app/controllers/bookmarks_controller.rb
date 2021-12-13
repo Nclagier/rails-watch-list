1e7fa06d20480a9cc858a14539e24c5fcbdd29d4
nathalie lagier <nclagier@gmail.com>
2021-12-13 17:14:04 -0400

    warch-list partie 1

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,25 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  private
+
+  def list_params
+    params.require(:bookmark).permit(:name)
+  end
+end
 end
